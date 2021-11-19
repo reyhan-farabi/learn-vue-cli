90994e8f2df2bfa82b214b5b2c0b2f7fb7a77cac
reyhan-farabi <reyhanfarabi@refactory.id>
2021-11-19 20:39:38 +0700

    change welcome page view

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <h3>Hello, Reyhan Farabi</h3>
+  <h1>Welcome to Vue JS</h1>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
+  components: {},
+  // data() {
+  //   return {
+  //     firstname: "Reyhan",
+  //     lastname: "Farabi",
+  //   };
+  // },
 };
 </script>
 
@@ -22,5 +25,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1 {
+  margin: 0;
+}
+
+h3 {
+  margin: 10px 0 0;
+  font-weight: 200;
 }
 </style>
